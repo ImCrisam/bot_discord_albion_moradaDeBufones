934461a9ed926ce153efb6f82ce620df086aad22
imcrisam <imcrisam@gmail.com>
2022-05-26 18:15:57 -0500

    strucure commands

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 //link invite https://discord.com/api/oauth2/authorize?client_id=977893992006045806&permissions=8&scope=bot
 require('dotenv').config()
+const commands = require('./src/commands/commands');
 
 const prefix = process.env.PREFIX
+
 
 const { Client, Intents } = require('discord.js');
 
@@ -9,12 +11,30 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 
 client.once('ready', (bot) => {
 	console.log(bot.presence.status);
+	client.user.setActivity(' ', { type: 'WATCHING' });
+	client.user.setStatus('online');
 });
 
-client.on("messageCreate", (msg)=>{
-	//console.log(msg);
-	console.log(msg.content.startsWith(prefix));
+client.on("messageCreate", (msg) => {
 
-} )
+	if (msg.content.startsWith(prefix) && msg.content.indexOf(" ") === prefix.length) {
+
+		const param = msg.content.slice(prefix.length).trim().split(/ +/);
+		const command = param.shift().toLocaleLowerCase();
+
+		console.log(param);
+		console.log(command);
+		try {
+			re1s = commands[command](msg, param)
+			console.log(re1s);
+			
+		} catch (error) {
+			console.log(`comando '${command}' no existe`);
+		}
+
+	}
+
+
+})
 
 client.login(process.env.DISCORD_TOKEN); 
---
+++ src/commands/commands.js
@@ -0,0 +1,10 @@
+module.exports ={
+    help : (msg, param) => {
+        return "Descripcion de usuario"
+    },
+
+    addguild : (msg, params) => {
+        return `parametros ${params}`
+    },
+
+}
